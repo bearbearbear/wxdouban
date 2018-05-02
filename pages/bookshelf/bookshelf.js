49b82bfc42938a420c779463ab5d4908c9f6843f
bearbearbear <ruiyi.chen@gmail.com>
2018-05-02 09:13:48 +0800

    bookshelf

--- pages/bookshelf/bookshelf.js
+++ pages/bookshelf/bookshelf.js
@@ -6,11 +6,21 @@ Page({
    */
   data: {
     books: [],
-    requestPath: 'https://www.easy-mock.com/mock/5acf2b009e1d8d12cbd79507/example/bookshelf?userid=',
-    resultCount: 0
+    requestPath: 'https://www.easy-mock.com/mock/5acf2b009e1d8d12cbd79507/example/bookshelf?username=',
+    resultCount: 0,
+    userInfo:{
+      avatarUrl: '../../images/profile.png',
+      nickName: '未登录'
+    },
+    bType: "primary", // 按钮类型
+    actionText: "登录",// 按钮提示文字
+    isLoggedIn: false,  // 登录按钮状态，false表示未登录
   },
 
   fetchBookList: function (targetURL) {
+    wx.showLoading({
+      title: 'loading...',
+    })
     let that = this
     wx.request({
       url: targetURL,
@@ -18,17 +28,24 @@ Page({
         'content-Type': 'application/json'
       },
       success: function (res) {
+        wx.hideLoading()
         if (res.statusCode == 200) {
           console.log(res.data)
-          if (res.data.books.length != 0) {
+          var resultCount =res.data.books.length;
+          if (resultCount != 0) {
+            
+            var books = res.data.books
+            
+            
+
             that.setData({
-              books: res.data.books,
-              isSearchResult: isSearchResult,
+              books: books,
               resultCount: res.data.books.length
             })
+            console.log(that.data);
 
             wx.setNavigationBarTitle({
-              title: '我的绘本'
+              title: '我的书架'
             })
           }
         }
@@ -42,7 +59,33 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.hideLoading();
+    // 获取本地数据中的用户信息
+    wx.getStorage({
+      key: 'userInfo',
+
+      // 能获取到就显示用户的信息，并保持登录状态，不能的话就什么都不做
+      success: (res) => {
+        wx.hideLoading();
+        this.setData({
+          userInfo: {
+            avatarUrl: res.data.userInfo.avatarUrl,
+            nickName: res.data.userInfo.nickName
+          },
+          bType: res.data.bType,
+          actionText: res.data.actionText,
+          isLoggedIn: true
+        }),
+        console.log(this.data)
+
+        
+        var bookPath = this.data.requestPath + this.data.userInfo.nickName
+        this.fetchBookList(bookPath)
+
+      }
+    })
+
+
   },
 
   /**
